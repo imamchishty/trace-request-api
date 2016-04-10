9d64488f64e966a25118ac0004f0d345849cadd2
Imam Chishty <imam.chishty@emc.com>
2016-04-10 09:19:30 +0400

    Updated the package names.

--- src/main/java/com/shedhack/trace/request/api/logging/handler/DefaultLoggingHandler.java
+++ src/main/java/com/shedhack/trace/request/api/logging/handler/DefaultLoggingHandler.java
@@ -1,4 +1,4 @@
-package com.shedhack.trace.request.api.handler;
+package com.shedhack.trace.request.api.logging.handler;
 
 import com.shedhack.trace.request.api.model.RequestModel;
 import org.slf4j.Logger;
